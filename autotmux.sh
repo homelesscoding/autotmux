eea846fc7fdd5262a2a123c2cf9d976272fe88a7
matt <46228173+matthernet@users.noreply.github.com>
2020-10-30 11:51:51 +0100

    Update autotmux.sh

--- autotmux.sh
+++ autotmux.sh
@@ -6,7 +6,7 @@ else
 	tmux new-session -d -s $1 #Create the new session with the name of the target-list
 	for i in `cat $1`;do
 		name=`echo $i | sed s/[.]/-/g`
-		tmux new-window -t $1 -n $name #Create the new window in session
-		tmux send-keys -t $1:$name "<command-to-execute> $i" C-m #Send the command to the windows
+		tmux new-window -t $1 -n $name #Create the new windows in session with the name of each target
+		tmux send-keys -t $1:$name "<command-to-execute> $i" C-m #Send the command and the targets to the windows
 	done
 fi
